df4fe148d75d8e52c01afae1233c862860663533
akagaeng <akagaeng@gmail.com>
2020-09-03 00:27:23 +0900

    feat: import functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,79 @@
+const getAchievementInfo = require('./functions/getAchievementInfo.js');
+const getAnnualReportDetail = require('./functions/getAnnualReportDetail.js');
+const getAnnualReportList = require('./functions/getAnnualReportList.js');
+const getBillAdditionalInfo = require('./functions/getBillAdditionalInfo.js');
+const getBillCommissionExaminationInfo = require('./functions/getBillCommissionExaminationInfo.js');
+const getBillComprehensiveAuditInfo = require('./functions/getBillComprehensiveAuditInfo.js');
+const getBillDeliverateInfo = require('./functions/getBillDeliverateInfo.js');
+const getBillFearInfo = require('./functions/getBillFearInfo.js');
 const getBillInfoList = require('./functions/getBillInfoList.js');
+const getBillPetitionMemberList = require('./functions/getBillPetitionMemberList.js');
+const getBillPreliminaryExaminationInfo = require('./functions/getBillPreliminaryExaminationInfo.js');
+const getBillReceiptInfo = require('./functions/getBillReceiptInfo.js');
+const getBillTransferredInfo = require('./functions/getBillTransferredInfo.js');
+const getCommitPetitionList = require('./functions/getCommitPetitionList.js');
+const getCommitPetitionMooringInfoList = require('./functions/getCommitPetitionMooringInfoList.js');
+const getCommitPetitionProcessInfoList = require('./functions/getCommitPetitionProcessInfoList.js');
+const getJsictionComiteProcessList = require('./functions/getJsictionComiteProcessList.js');
+const getJudgeReportBodyList = require('./functions/getJudgeReportBodyList.js');
+const getMooringBillStaticList = require('./functions/getMooringBillStaticList.js');
+const getMotionLawList = require('./functions/getMotionLawList.js');
+const getOfferReasonList = require('./functions/getOfferReasonList.js');
+const getPassRceptRecentList = require('./functions/getPassRceptRecentList.js');
+const getPeriodList = require('./functions/getPeriodList.js');
+const getPetitionAdditionalInfo = require('./functions/getPetitionAdditionalInfo.js');
+const getPetitionCommissionExaminationInfo = require('./functions/getPetitionCommissionExaminationInfo.js');
+const getPetitionDeliverateInfo = require('./functions/getPetitionDeliverateInfo.js');
+const getPetitionList = require('./functions/getPetitionList.js');
+const getPetitionReceiptInfo = require('./functions/getPetitionReceiptInfo.js');
+const getPetitionStatic = require('./functions/getPetitionStatic.js');
+const getPetitionTransferredInfo = require('./functions/getPetitionTransferredInfo.js');
+const getProcessBillStaticInfoList = require('./functions/getProcessBillStaticInfoList.js');
+const getRceptKndList = require('./functions/getRceptKndList.js');
+const getRceptMoorList = require('./functions/getRceptMoorList.js');
+const getRecentMoorList = require('./functions/getRecentMoorList.js');
+const getRecentPasageList = require('./functions/getRecentPasageList.js');
 const getRecentRceptList = require('./functions/getRecentRceptList.js');
+const getSessionRequestList = require('./functions/getSessionRequestList.js');
+
 
 module.exports = {
+  getAchievementInfo,
+  getAnnualReportDetail,
+  getAnnualReportList,
+  getBillAdditionalInfo,
+  getBillCommissionExaminationInfo,
+  getBillComprehensiveAuditInfo,
+  getBillDeliverateInfo,
+  getBillFearInfo,
   getBillInfoList,
-  getRecentRceptList
+  getBillPetitionMemberList,
+  getBillPreliminaryExaminationInfo,
+  getBillReceiptInfo,
+  getBillTransferredInfo,
+  getCommitPetitionList,
+  getCommitPetitionMooringInfoList,
+  getCommitPetitionProcessInfoList,
+  getJsictionComiteProcessList,
+  getJudgeReportBodyList,
+  getMooringBillStaticList,
+  getMotionLawList,
+  getOfferReasonList,
+  getPassRceptRecentList,
+  getPeriodList,
+  getPetitionAdditionalInfo,
+  getPetitionCommissionExaminationInfo,
+  getPetitionDeliverateInfo,
+  getPetitionList,
+  getPetitionReceiptInfo,
+  getPetitionStatic,
+  getPetitionTransferredInfo,
+  getProcessBillStaticInfoList,
+  getRceptKndList,
+  getRceptMoorList,
+  getRecentMoorList,
+  getRecentPasageList,
+  getRecentRceptList,
+  getSessionRequestList,
 };
 
