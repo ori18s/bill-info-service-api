e7ec443b5cf5121554cfbd7299ffa8ebd5f6137f
akagaeng <akagaeng@gmail.com>
2020-09-03 23:21:29 +0900

    chore: edit type name

--- src/functions/getRecentPasageList.js
+++ src/functions/getRecentPasageList.js
@@ -2,7 +2,7 @@ const shared = require('../shared');
 
 
 /**
- * @typedef GetRecentPasageList
+ * @typedef GetRecentPasageListParam
  * @property {String} pageNo: pageNo: '1',
  * @property {String} numOfRows: '10',
  * @property {String} bill_name: '결의안',
@@ -12,7 +12,7 @@ const shared = require('../shared');
  * 최근 통과의안 목록을 조회
  * @param {String} baseUrl - openapi base url
  * @param {String} serviceKey - openapi serviceKey
- * @param {GetRecentPasageList} params - openapi params: query string supported from open api
+ * @param {GetRecentPasageListParam} params - openapi params: query string supported from open api
  */
 module.exports = (baseUrl, serviceKey, params) => {
   const resource = shared.getBasePathFromFilename(__filename);
