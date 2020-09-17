ff5599736c6ff0234e1de48797a6ff4600b6e0d2
akagaeng <akagaeng@gmail.com>
2020-09-17 12:09:52 +0900

    fix: move dotenv files onto config

---
+++ config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  basePath: 'http://apis.data.go.kr/9710000/BillInfoService2/',
+  serviceKey: process.env.SERVICE_KEY
+};
--- tests/getRecentRceptList.test.js
+++ tests/getRecentRceptList.test.js
@@ -1,15 +1,16 @@
 const getRecentRceptList = require('../src/functions/getRecentRceptList');
+const {basePath, serviceKey} = require('../config');
 const params = {
-    pageNo: '1',
-    numOfRows: '10',
-    bill_name: '국회법'
+  pageNo: '1',
+  numOfRows: '10',
+  bill_name: '국회법'
 };
 
 beforeAll(() => {
-    require('dotenv').config();
+  require('dotenv').config();
 });
 
 it('getRecentRceptList', async () => {
-    const res = await getRecentRceptList(process.env.OPEN_API_BASE_ENDPOINT, process.env.SERVICE_KEY, params);
-    expect(res[0].billId.split('_')[0]).toBe('PRC');
+  const res = await getRecentRceptList(basePath, serviceKey, params);
+  expect(res[0].billId.split('_')[0]).toBe('PRC');
 });
