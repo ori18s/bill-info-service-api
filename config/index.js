9f9ab868836fee142892b6cb5dde2817dd4fdc82
akagaeng <akagaeng@gmail.com>
2020-09-17 14:17:48 +0900

    feat: setup config

--- config/index.js
+++ config/index.js
@@ -1,4 +1,17 @@
-module.exports = {
-  basePath: 'http://apis.data.go.kr/9710000/BillInfoService2/',
-  serviceKey: process.env.SERVICE_KEY
+const c = require('ansi-colors');
+
+module.exports = () => {
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+  console.log('NODE_ENV:', NODE_ENV);
+
+  if (NODE_ENV !== 'production') {
+    console.log(c.yellow('* dotenv activated @config'));
+    require('dotenv').config();
+  }
+
+  return {
+    nodeEnv: NODE_ENV,
+    basePath: 'http://apis.data.go.kr/9710000/BillInfoService2/',
+    serviceKey: process.env.SERVICE_KEY
+  }
 };
