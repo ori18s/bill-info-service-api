83e3492cf664de8f5064e1042c1a5c661c226f6b
akagaeng <akagaeng@gmail.com>
2020-09-17 13:26:42 +0900

    feat: include basePath, serviceKey in the shared.get function

--- src/shared/index.js
+++ src/shared/index.js
@@ -3,20 +3,21 @@ const path = require('path');
 
 /**
  * @description shared function for openapi http request
- * @param {String} url - openapi url with resource
- * @param {String} serviceKey - open api service key
- * @param {Object} params - querystring openapi supports
+ * @param {String} resourcePath - resource path without basePath
+ * @param {Object} params - querystring openapi suppathports
  */
-exports.get = async (url, serviceKey, params) => {
+exports.get = async (resourcePath = '', params) => {
+  const {basePath, serviceKey} = require('../../config')();
+
   if (!serviceKey) {
     throw new Error('Service key required. Set as an environment variable.');
   }
   axios.defaults.adapter = require('axios/lib/adapters/http');
-  return axios.get(`${url}?ServiceKey=${serviceKey}`, params)
+  return axios.get(`${basePath}${resourcePath}?ServiceKey=${serviceKey}`, params)
       .then(res => res.data.response.body.items.item)
       .catch(err => err);
 };
 
 exports.getBasePathFromFilename = (currentPath = __filename, ext = '.js') => {
   return path.basename(currentPath, ext);
-}
+};
