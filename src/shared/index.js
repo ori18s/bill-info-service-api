6dd8888171b4b38059da89a5821ea4c92c5268bc
akagaeng <akagaeng@gmail.com>
2020-09-18 17:17:24 +0900

    fix: querystring 제대로 들어가지 않고있던 것을 수정

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const path = require('path');
+const qs = require('querystring');
 
 /**
  * @description shared function for openapi http request
@@ -13,7 +14,21 @@ exports.get = async (resourcePath = '', params) => {
     throw new Error('Service key required. Set as an environment variable.');
   }
   axios.defaults.adapter = require('axios/lib/adapters/http');
-  return axios.get(`${basePath}${resourcePath}?ServiceKey=${serviceKey}`, params)
+
+  let queryString = `ServiceKey=${serviceKey}`;
+
+  const queryParam = {
+    ...params
+  };
+  const query = qs.stringify(queryParam);
+
+  if (query) {
+    queryString += `&${query}`
+  }
+
+  const url = `${basePath}${resourcePath}?${queryString}`;
+
+  return axios.get(url)
       .then(res => res.data.response.body.items.item)
       .catch(err => err);
 };
